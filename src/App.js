3a2cec9d82159cd5dc9277f971727593f8f73bfa
Chajin Oh <paran_piano@hotmail.com>
2020-12-25 12:39:58 +0900

    Add Component - Content , Article

--- src/App.js
+++ src/App.js
@@ -12,10 +12,44 @@ class Subject extends Component {
   }
 }
 
+class Toc extends Component {
+  render() {
+   return(
+    <nav>
+    <ul>
+        <li>
+            <li><a href="1.html"></a>HTML</li>
+            <li><a href="2.html"></a>CSS</li>
+            <li><a href="3.html"></a>JavaScript</li>
+        </li>
+    </ul>
+  </nav>
+
+   );
+ }
+}
+
+class Content extends Component {
+  render() {
+   return(
+    <ariticle>
+    <h2>
+        HTML2
+    </h2>
+    HTML is Hyper Text Markup Lagnuage.
+    </ariticle>
+
+   );
+ }
+}
+
+
 function App() {
   return (
     <div className="App">
       <Subject></Subject>
+      <Toc></Toc>
+      <Content></Content>
     </div>
   );
 }
