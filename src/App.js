c55b98a92d8c3748e3c3f657c1044f7133b65c23
Chajin Oh <paran_piano@hotmail.com>
2020-12-25 12:35:49 +0900

    Subject Component - Header Tag

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
+import React , { Component } from 'react'
 import './App.css';
+
+class Subject extends Component {
+   render() {
+    return(
+      <header>
+        <h1>WEB</h1>
+        world wide web! 
+      </header>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      Hello, React!
+      <Subject></Subject>
     </div>
   );
 }
