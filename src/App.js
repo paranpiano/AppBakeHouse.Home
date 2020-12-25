f25bfa5e910eb979ac819b0352f891dd8af20b23
Chajin Oh <paran_piano@hotmail.com>
2020-12-25 13:46:18 +0900

    state - 복수 데이타 전달 key를 생성

--- src/App.js
+++ src/App.js
@@ -9,7 +9,12 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      subject:{title:'WEB', sub:'World Wide Web!'}
+      subject:{title:'WEB', sub:'World Wide Web!'},
+      contents: [
+        {id: 1, title : 'HTML', desc: 'HTML is HyperText ...'},
+        {id: 2, title : 'CSS', desc: 'CSS is for design'},
+        {id: 3, title : 'JAvaScrip', desc: 'JavaScript is for interactive'}
+      ]
     }
   }
 
@@ -17,7 +22,7 @@ class App extends Component {
     return (
       <div className="App">
         <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
-        <TOC></TOC>
+        <TOC data = {this.state.contents}></TOC>
         <Content title='HTML' desc="HypterType markup Language"></Content>
       </div>
     );
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -2,18 +2,24 @@ import React , { Component } from 'react'
 
 class TOC extends Component {
     render() {
+
+     var lists = [];
+     var data = this.props.data;
+     var i = 0;
+
+     while(i < data.length){
+        lists.push(<li key={data[i].id}>
+            <a href={"/content/"+data[i].id}>{data[i].title}</a></li>);
+        i = i + 1;
+     }
+
      return(
-      <nav>
-      <ul>
-          <li>
-              <li><a href="1.html"></a>HTML</li>
-              <li><a href="2.html"></a>CSS</li>
-              <li><a href="3.html"></a>JavaScript</li>
-          </li>
-      </ul>
-    </nav>
-  
-     );
+        <nav>
+            <ul>
+                {lists}
+            </ul>
+        </nav>
+       );
    }
   }
 
