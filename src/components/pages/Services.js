4393626ec5552667ed6c5fe56fd15b42e2e7f41c
Chajin Oh <paran_piano@hotmail.com>
2021-11-15 16:27:04 +0900

    Update service list

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -8,21 +8,36 @@ const Services = () => {
         <h2>Mobile Apps</h2>
         <h4>
           <a href="https://play.google.com/store/apps/details?id=com.appbakehouse.qr_scan_app">
-            QR & Scanner on map
+            DONUT - AI QR SCAN
+          </a>
+        </h4>
+        <h4>
+          <a href="https://play.google.com/store/apps/details?id=com.companyname.ui_thebestprice">
+            BAGLE - AI WISH LIST
+          </a>
+        </h4>
+        <h4>
+          <a href="https://play.google.com/store/apps/details?id=com.crossbim.mobileviewer">
+            CROSSTEAM - FRONTEND DEVELOPMENT
+          </a>
+        </h4>
+        <h4>
+          <a href="">
+            REVIEWNOTE - BACKEND DEVELOPMENT(TBD)
           </a>
         </h4>
         <h2>Web Apps</h2>
         <h4>
-          <a href="https://play.google.com/store/apps/details?id=com.appbakehouse.qr_scan_app">
+          {/* <a href="https://play.google.com/store/apps/details?id=com.appbakehouse.qr_scan_app">
             Airbnb - booking data
-          </a>
+          </a> */}
         </h4>
 
         <h2>Machine Learning Services</h2>
         <h4>
-          <a href="https://play.google.com/store/apps/details?id=com.appbakehouse.qr_scan_app">
+          {/* <a href="https://play.google.com/store/apps/details?id=com.appbakehouse.qr_scan_app">
             QR Prediction Service
-          </a>
+          </a> */}
         </h4>
       </div>
     </ImageContents>
